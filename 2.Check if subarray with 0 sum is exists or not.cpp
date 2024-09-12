5efbc67fcaf72aa1de87050cb69b361ca70abb86
Deepak kumar <87894208+Deepak6o@users.noreply.github.com>
2024-09-12 19:10:20 +0530

    Update 2.Check if subarray with 0 sum is exists or not.cpp

--- 2.Check if subarray with 0 sum is exists or not.cpp
+++ 2.Check if subarray with 0 sum is exists or not.cpp
@@ -1,41 +1,54 @@
+// Check if a subarray with 0 sum exists or not
+
+
 #include<iostream>
+#include <algorithm>
 #include<unordered_set>
 using namespace std;
-bool checkZero(int arr[], int n)
-{
-   /* for(int i=0;i<n-1;i++){
-int sum=0;
-        for(int j=i;j<n;j++)
-        {
-            sum+=arr[j];
-            if(sum==0){
-                cout<<"Subarray with zero sum exits";
-                return;
-            }
 
-        }
-    }*/
-    unordered_set<int>s;
-    s.insert(0);
-    int sum=0;
-    for(int i=0;i<n;i++)
-    {
-        sum+=arr[i];
-        if(s.find(sum)!=s.end())
-        {
-            return true;
-        }
-        else{
-            s.insert(sum);
-        }
+//1. Using Brute-Force
+// void Subarray_Zero(int n, int arr[]){
+//    for(int i=0;i<n;i++)
+//    {
+//     int sum=0;
+//     for(int j=i;j<n;j++)
+//     {
+//         sum=sum+arr[j];
+//         if(sum==0){
+//             cout<<"Subarray_Exit";
+//             return;
+//         }
+//     }
+//    }
+//    cout<<"Not Exit";
+// }
+
+//1. Using STL
+void Subarray_Zero(int n, int arr[]){
+   unordered_set<int> s;
+   
+   int sum=0;
+   for(int i=0;i<n;i++){
+    sum+=arr[i];
+    if(s.find(sum)!=s.end()){
+        cout<<"Subarray_Zero";
+        return;
     }
-    return false;
-
+    else{
+        s.insert(sum);
+    }
+   }
+   cout<<"Not Exit";
 }
-int main()
-{
-    int n=10;
-    int arr[10]={3,4,-7,3,1,3,1,-4,-2,-2};
-    checkZero(arr,n)?cout<<"Subarray exit" : cout<<"Subarray not exit";
 
+int main(){
+   int n,target;
+   cout<<"size = ";
+   cin>>n;
+   int arr[n];
+   for(int i=0;i<n;i++)
+   {
+    cin>>arr[i];
+   }
+   Subarray_Zero(n,arr);
 }
