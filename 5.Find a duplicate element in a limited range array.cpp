7fbc7cfbe012ff36964b41ec962f4e3315da9177
Deepak kumar <deepak21062000@gmail.com>
2024-09-17 09:08:50 +0530

    5.Find a duplicate element in a limited range array.cpp

--- 5.Find a duplicate element in a limited range array.cpp
+++ 5.Find a duplicate element in a limited range array.cpp
@@ -1,35 +1,70 @@
+//Find the duplicate element in a limited range array
 #include<iostream>
-#include<bits/stdc++.h>
+#include<unordered_map>
+#include<algorithm>
+#include <numeric>
+#include<vector>
 using namespace std;
-int FindDupicate(vector<int>nums, int n)
-{
-   /* unordered_map<int,int> mp;
+
+//O(nlogn)
+void Duplicate(int n, int arr[]){
+    sort(arr,arr+n);
     for(int i=0;i<n;i++)
     {
-        mp[nums[i]]++;
-    }
-
-    for(auto x: mp)
-    {
-        if(x.second>1){
-            return x.first;
+        if(arr[i]==arr[i+1])
+        {
+            cout<<arr[i];
+            return;
         }
-    }
-    return 0;*/
-    vector<bool> visited(n);
-    for(auto x: nums)
-    {
-        if(visited[x]){
-            return x;
-        }
-        visited[x]=true;
-    }
+    }  
 }
+
+// O(n)
+// int Duplicate(int n, int arr[]){
+//     unordered_map<int,int> map;
+//     for(int i=0;i<n;i++)
+//     {
+//         map[arr[i]]++;
+//     }
+//     for(auto x: map){
+//         if(x.second>1){
+//             return x.first;
+//         }
+//     }
+// }
+
+//Using Difference in Sum O(n);
+// void Duplicate(int n, int arr[]){
+//     int expected_sum = n*(n-1)/2;
+//     int actual_sum = 0;
+//     for(int i=0;i<n;i++)
+//     {
+//         actual_sum+=arr[i];
+//     }
+//     cout<<actual_sum-expected_sum;
+// }
+
+// Using XOR O(n)
+// void Duplicate(int n, int arr[]){
+//     int Xor=0;
+//     for(int i=0;i<n;i++)
+//     {
+//         Xor^=arr[i];
+//     }
+//     for(int i=1;i<=n-1;i++)
+//     {
+//         Xor^=i;
+//     }
+//     cout<<Xor;
+// }
+
+
+
+
 
 int main()
 {
-    vector<int>nums={1, 2, 3, 4, 4};
-    int n=nums.size();
-    cout<<FindDupicate(nums,n);
+    int arr[] = { 1, 2, 3, 4, 4  };
+    int n= sizeof(arr)/sizeof(arr[0]);
+    Duplicate(n,arr);
 }
-
